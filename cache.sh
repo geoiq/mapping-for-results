5e6ef6e3c09e41eee13b4eec591a9313cd033c1e
Andrew Turner <andrew@highearthorbit.com>
2010-10-06 03:06:17 -0400

    Added a loading screen

--- cache.sh
+++ cache.sh
@@ -7,5 +7,5 @@ mkdir -p eap/
 curl http://localhost:4567/lac/haiti > lac/haiti.html
 curl http://localhost:4567/lac/bolivia > lac/bolivia.html
 curl http://localhost:4567/afr/kenya > afr/kenya.html
-curl http://localhost:4567/eap/phillipines > eap/phillipines.html
+curl http://localhost:4567/eap/philippines > eap/philippines.html
 curl http://localhost:4567/about > about.html
