f815b8c6cd09f4280c1e29ce7ca050128a875698
Andrew Turner <andrew@highearthorbit.com>
2011-01-12 09:29:49 -0500

    Financing for to Active Portfolio for... text changes in JS

--- map.js
+++ map.js
@@ -481,7 +481,7 @@ if(typeof(F1)=='undefined') {F1 = {};}
                  
             var financing_total = funding > 1000 ? (funding/1000).toFixed(2) + " Billion" : funding.toFixed(2) + " Million";
             jq('#sector_funding_total').html("$" + financing_total); // + " <span class='subtotal' title='Global Financing'>/ $136.912 Billion</span>"
-            jq('#sector_funding_title').html("Financing for " + sector_names + " Sectors")
+            jq('#sector_funding_title').html("Active Portfolio for " + sector_names + " Sectors")
 
         } else {
             projects = self.sectors[sector_name].projects;
@@ -502,7 +502,7 @@ if(typeof(F1)=='undefined') {F1 = {};}
             var financing_total = self.total_funding > 1000 ? (self.total_funding/1000).toFixed(2) + " Billion" : self.total_funding.toFixed(2) + " Million"
 
             jq('#sector_funding_total').html("$" + funding.toFixed(2) + " Million <span class='subtotal' title='National Financing'>/ $"+ financing_total + "</span>");
-            jq('#sector_funding_title').html("Financing for " + sector_names + " Sector")
+            jq('#sector_funding_title').html("Active Portfolio for " + sector_names + " Sector")
         }
 
            if(projects.length == 0){
