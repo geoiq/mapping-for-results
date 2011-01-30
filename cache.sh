753616c9d620d0b53f3cb465509402da963a0f54
Andrew Turner <andrew@highearthorbit.com>
2011-01-30 18:40:34 -0500

    Updated the Sync for Kenya, Bolivia, and Philippines

--- cache.sh
+++ cache.sh
@@ -1,23 +1,25 @@
 #!/usr/bin/env bash
 
-curl http://localhost:4567/ > index.html
-curl http://localhost:4567/404 > 404.html
-curl http://localhost:4567/500 > 500.html
+#curl http://localhost:4567/ > index.html
+#curl http://localhost:4567/404 > 404.html
+#curl http://localhost:4567/500 > 500.html
 curl http://localhost:4567/map.js > map.js
-mkdir -p lac/
-mkdir -p afr/
-mkdir -p eap/
+#mkdir -p lac/
+#mkdir -p afr/
+#mkdir -p eap/
+#mkdir -p sar/
 curl http://localhost:4567/lac/haiti > lac/haiti.html
 curl http://localhost:4567/lac/bolivia > lac/bolivia.html
 curl http://localhost:4567/afr/kenya > afr/kenya.html
-curl http://localhost:4567/lac > lac/index.html
-curl http://localhost:4567/afr > afr/index.html
-cp lac/index.html lac.html
-cp afr/index.html afr.html
+#curl http://localhost:4567/lac > lac/index.html
+#curl http://localhost:4567/afr > afr/index.html
+#cp lac/index.html lac.html
+#cp afr/index.html afr.html
 curl http://localhost:4567/eap/philippines > eap/philippines.html
-curl http://localhost:4567/about > about.html
+#curl http://localhost:4567/about > about.html
+curl http://localhost:4567/sar/nepal > sar/nepal.html
 
-mkdir -p productline/PE/
-curl http://localhost:4567/productline/PE/charts > productline/PE/charts.html
-mkdir -p productline/CN/
-curl http://localhost:4567/productline/CN/charts > productline/CN/charts.html
+#mkdir -p productline/PE/
+#curl http://localhost:4567/productline/PE/charts > productline/PE/charts.html
+#mkdir -p productline/CN/
+#curl http://localhost:4567/productline/CN/charts > productline/CN/charts.html
