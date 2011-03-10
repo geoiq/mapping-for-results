0aa633ac74020bce7af97d8351a18923c60767eb
Andrew Turner <andrew@geoiq.com>
2011-03-09 19:31:41 -0500

    Fix region pages to use data elements

--- m4r/server.rb
+++ m4r/server.rb
@@ -126,8 +126,8 @@ get '/:region' do
   elsif(@page.page_type == "page")
     erb :wiki
   else
-    @projects = @page.projects[:projects]
-    @financing  = @page.projects[:financing]
+    @projects = @page.data[:projects]
+    @financing  = @page.data[:financing]
     erb :index
   end
 end
